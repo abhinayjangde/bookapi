d4041b73bcfd67f31ffb5ce859740346d0ca925e
Abhinay Jangde <abhinayjangde@gmail.com>
2025-09-14 09:08:16 +0530

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        message: "I am healthy!",
+    });
+});
+
+export default app;
