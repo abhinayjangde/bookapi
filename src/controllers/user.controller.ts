3ac11bf0b3b9b454a716c6b72f9878796fcebad4
Abhinay Jangde <abhinayjangde@gmail.com>
2025-09-15 11:03:48 +0530

    password hashing is done

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
+import UserModel from "../models/user.model.js";
 
 export const registerUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -12,8 +13,18 @@ export const registerUser = async (req: Request, res: Response, next: NextFuncti
             return next(error);
         }
 
+        // 3. Check if user already exists
+        const existingUser = await UserModel.findOne({ email });
 
-        res.status(201).json({ message: "User registered successfully.", user: "newUser" });
+        if (existingUser) {
+            const error = createHttpError(409, "User already exists with this email.");
+            return next(error);
+        }
+
+        // 4. Create a new user
+        const newUser = await UserModel.create({ name, email, password });
+
+        res.status(201).json({ message: "User registered successfully.", user: newUser._id });
     } catch (error) {
         next(error);
     }
---
+++ src/models/user.model.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import type { User } from "../types/user.types.js";
+import bcrypt from "bcryptjs";
+
+
+const userSchema = new mongoose.Schema<User>(
+    {
+        name: { type: String, required: true, unique: true, trim: true },
+        email: { type: String, required: true, unique: true, trim: true },
+        password: { type: String, required: true },
+    },
+    { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+
+    if (!this.isModified("password")) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+
+});
+
+const User = mongoose.model<User>("User", userSchema);
+
+export default User;
