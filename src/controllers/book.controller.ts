18770afee6b447850df3e735c462a30e9ac61381
Abhinay Jangde <abhinayjangde@gmail.com>
2025-09-19 12:27:22 +0530

    add file upload using multer

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -2,6 +2,10 @@ import type { NextFunction, Request, Response } from 'express';
 
 export const createBook = (req: Request, res: Response, next: NextFunction) => {
     try {
+
+
+        console.log(req.files);
+
         res.send('Create a new book');
     } catch (error) {
         next(error);
--- src/routes/book.route.ts
+++ src/routes/book.route.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import multer from "multer";
+import path from "node:path";
 import { createBook } from "../controllers/book.controller.js";
-import upload from "../middlewares/upload.middleware.js";
 
 const router = express.Router();
+
+const upload = multer({
+    dest: path.resolve() + "\\public\\data\\uploads",
+    limits: { fileSize: 30 * 1024 * 1024 }, // 30MB (3e7 bytes)
+});
 
 router.post("/add", upload.fields([
     { name: "coverImage", maxCount: 1 },
