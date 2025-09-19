1424a0337b333071bc1b180c2939f691b211d5a6
Abhinay Jangde <abhinayjangde@gmail.com>
2025-09-19 21:37:34 +0530

    add book cover

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,12 @@ const _config = {
     port: process.env.PORT || 3000,
     dbUri: process.env.DB_URI || "mongodb://root:root@localhost:27017/bookapi?authSource=admin&w=1",
     env: process.env.NODE_ENV,
-    jwtSecret: process.env.JWT_SECRET
+    jwtSecret: process.env.JWT_SECRET,
+    cloudinary: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+    },
 }
 
 export const config = Object.freeze(_config);
--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,11 +1,28 @@
 import type { NextFunction, Request, Response } from 'express';
+import cloudinary from '../config/cloudinary.js';
+import path from "node:path";
+import createHttpError from 'http-errors';
 
-export const createBook = (req: Request, res: Response, next: NextFunction) => {
+export const createBook = async (req: Request, res: Response, next: NextFunction) => {
     try {
 
+        const files = req.files as { coverImage?: Express.Multer.File[] };
 
-        console.log(req.files);
+        const coverImage = files?.coverImage?.[0];
 
+        if (!coverImage) {
+            return next(createHttpError(400, "Cover image is required."));
+        }
+        const coverImageMimeType = coverImage.mimetype.split('/')[1] ?? 'jpg';
+        const fileName = coverImage.filename;
+        const filePath = path.join(process.cwd(), "public", "data", "uploads", fileName);
+
+        const uploadResult = await cloudinary.uploader.upload(filePath, {
+            filename_override: fileName,
+            folder: "book-covers",
+            format: coverImageMimeType
+        })
+        console.log(uploadResult);
         res.send('Create a new book');
     } catch (error) {
         next(error);
